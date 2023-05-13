ced9631b3c67f13bc8f5fe9b20bd20b9308d30ee
OTHmanera <raisothmane4@gmail.com>
2023-05-13 22:17:07 +0100

    v5

--- gestion_stagiaire_ocp/resources/views/admin/valider.blade.php
+++ gestion_stagiaire_ocp/resources/views/admin/valider.blade.php
@@ -33,9 +33,9 @@
                         <td class="border px-4 py-2">{{ $stg->departement }} </td>
                         <td class="border px-4 py-2">{{ $stg->periode }} mois</td>
                         <td class="border px-4 py-2">{{ $stg->ecole }}</td>
-                        <td class="border px-4 py-2">
+                        <td class="border px-4 py-2 ">
 
-                                    <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+                                    <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center mr">
             <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
             <a href="{{ route('generate.pdf', ['id' => $stg->id, 'timestamp' => time()]) }}"
             >
